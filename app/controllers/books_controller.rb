d07596a570eb424748bdff2d441a71fd5073d31c
satos310 <119788681+satos310@users.noreply.github.com>
2023-02-28 12:32:35 +0000

    [fix book_index]task3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
-  def new
-    @books = Book.new
-  end
 
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      # index.htmlに繋ぐ
+      @books = Book.all
+      # viewをindexに戻す
+      render :index
+    end
+  end
+  
   def index
     @user = current_user
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
@@ -16,7 +29,7 @@ class BooksController < ApplicationController
   private
   
   def book_params
-    params.rewuire(:book).permit(:image, :title, :body)
+    params.require(:book).permit(:image, :title, :body)
   end
   
 end
--- db/migrate/20230221191137_create_books.rb
+++ db/migrate/20230221191137_create_books.rb
@@ -1,8 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
-      t.timestamps
+      t.references :user, foreign_key: true
+      t.string :title
+      t.string :introduction
     end
   end
 end
